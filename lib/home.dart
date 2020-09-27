60dd86da48820e2338b233561cff3f6287b84f82
Julius Karl <julius.karl.macrohon@gmail.com>
2020-09-28 02:19:34 +1300

    Tracked Tabs

--- lib/home.dart
+++ lib/home.dart
@@ -8,30 +8,56 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  int _activeTabIndex;
+  List<Tab> myTabs = <Tab>[
+    Tab(text: "Lap"),
+    Tab(text: "Random"),
+    Tab(text: "Interval"),
+  ];
+  TabController _tabController;
+
+  void _setActiveTabIndex() {
+    _activeTabIndex = _tabController.index;
+    print(_activeTabIndex);
+  }
+
+  @override
+  void initState() {
+    _tabController = TabController(vsync: this, length: myTabs.length);
+    _tabController.addListener(_setActiveTabIndex);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-              backgroundColor: Colors.grey,
-              title: Text("Reflex"),
-              centerTitle: true),
-          bottomNavigationBar: BottomAppBar(
-            child: TabBar(
-              labelColor: Colors.grey,
-              indicatorColor: Colors.grey,
-              tabs: [
-                Tab(icon: Icon(Icons.access_alarm), text: "Lap"),
-                Tab(icon: Icon(Icons.help_outline), text: "Random"),
-                Tab(icon: Icon(Icons.notifications_none), text: "Interval"),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [Lap(), Random(), Round()],
-          ),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Reflex"),
+          centerTitle: true,
+        ),
+        bottomNavigationBar: TabBar(
+          indicatorColor: Colors.lightBlue,
+          labelColor: Colors.lightBlue,
+          unselectedLabelColor: Colors.grey,
+          controller: _tabController,
+          tabs: myTabs,
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: myTabs.map((Tab tab) {
+            final String label = tab.text.toLowerCase();
+            return Center(
+              child: Text('$label'),
+            );
+          }).toList(),
         ));
   }
 }
